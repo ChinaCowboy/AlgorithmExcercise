6331dab87b3ba38464c91c98d8a08b95723995a1
Bing NIU <BNiu@slb.com>
2020-05-09 18:24:43 +0800

    add more

---
+++ Bent/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Bent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string str = "1211918";
+            int n = 2;
+            var results = RemoveNumberForSmallest.buildLowestNumber(str, n);
+            Console.WriteLine(results);
+            Console.ReadLine();
+        }
+    }
+
+    public class RemoveNumberForSmallest
+    {
+       // public static string results = "";
+        public static string buildLowestNumber(string str, int n)
+        {
+            var results = "";
+            if (n == 0)
+            {
+                results += str;
+                return results;
+            }
+            int len = str.Length;
+
+            if (len <= n)
+            {
+                return results;
+            }
+
+            int minIndex = 0;
+            // Find the smallest character among  
+            // first (n+1) characters of str.
+
+            for (int i = 1; i <=n ; i++)
+            {
+                if (str[i] < str[minIndex])
+                    minIndex = i;
+
+            }
+            // Append the smallest character to result  
+            results += str[minIndex];
+
+            string new_string = str.Substring(minIndex + 1);
+
+            results+= buildLowestNumber(new_string, n - minIndex);
+
+            return results;
+        }
+
+
+
+    }
+}
---
+++ GraphFriends/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace GraphFriends
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            char[][] friends = { "YYNN".ToCharArray(), "YYYN".ToCharArray(), "NYYN".ToCharArray(), "NNNY".ToCharArray() };
+            Console.WriteLine(getFriendCircles(friends));
+
+        }
+
+        public static int getFriendCircles(char[][] friends)
+        {
+            if (friends == null || friends.Length < 1)
+                return 0;
+            int numOfCircles = 0;
+            bool[] visited = new bool[friends.Length];
+            for (int i = 0; i < visited.Length; i++)
+                visited[i] = false;
+
+            for(int i = 0; i <friends.Length;i++)
+            {
+                if (!visited[i])
+                {
+                    numOfCircles++;
+                    visited[i] = true;
+                    findFriends(friends, visited, i);
+                   
+                    
+                }
+            }
+            return numOfCircles;
+        }
+
+        private static void findFriends(char[][] friends, bool[] visited, int ind)
+        {
+            for(int i=0; i<friends.Length;i++)
+            {
+                if (!visited[i] && i !=ind && friends[ind][i]=='Y')
+                {
+                    visited[i] = true;
+                    findFriends(friends, visited, i);
+                }
+            }
+        }
+    }
+}
